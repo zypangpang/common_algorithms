6c33513dc74cbc6a7498f43a702e1bd32a420621
zypang <pangzaiyu@163.com>
2020-02-17 21:12:35 +0800

    max flow Edmonds-Karp

--- main.cpp
+++ main.cpp
@@ -15,83 +15,66 @@ void printVec(vector<T> const& v){
 	copy(v.begin(),v.end(),ostream_iterator<T>(cout," "));
 }
 const int MAXINT=numeric_limits<int>::max();
-struct Node{
-	int u,d;
+struct Edge{
+	int u,v,cap,flow;
 };
-vector<vector<Node>> G;
-vector<int> d;
-vector<int> p;
-vector<bool> inque;
-vector<int> cnt;
-void readGraph(){
-	int m,n;
-	cin>>m>>n;
-	G.resize(m);
-	d.resize(m,MAXINT);
-	p.resize(m);
-	cnt.resize(m);
-	inque.resize(m);
-	for(int i=0;i<n;++i){
-		int u,v,d;
-		cin>>u>>v>>d;
-		G[u].push_back(Node{v,d});
-	}
+ostream& operator<<(ostream& out, Edge const& e){
+	cout<<"("<<e.u<<","<<e.v<<","<<e.cap<<","<<e.flow<<")";
+	return out;
 }
-bool SPFA(int u){
-	queue<int> q;
-	q.push(u);
-	inque[u]=1;
-	d[u]=0;
-	p[u]=-1;
-	int n=G.size();
-	while(!q.empty()){
-		auto u=q.front();q.pop();
-		inque[u]=0;
-		for(auto const& node: G[u]){
-			auto v=node.u;
-			auto dist=node.d;
-			if(d[u]<MAXINT && d[u]+dist<d[v]){
-				d[v]=dist+d[u];
-				p[v]=u;
-				if(!inque[v]){
-					q.push(v);
-					inque[v]=1;
-					if(++cnt[v]>n) return false;
+vector<Edge> edges;
+vector<vector<int>> G;
+vector<int> a;
+vector<int> p;
+void addEdge(int u,int v,int c){
+	edges.push_back(Edge{u,v,c,0});
+	edges.push_back(Edge{v,u,0,0});
+	G[u].push_back(edges.size()-2);
+	G[v].push_back(edges.size()-1);
+}
+int maxFlow(int s,int t){
+	int flow=0;
+	while(1){
+		fill(a.begin(),a.end(),0);
+		a[s]=MAXINT;
+		queue<int> Q;
+		Q.push(s);
+		while(!Q.empty()){
+			auto u=Q.front();Q.pop();
+			for(auto eid: G[u]){
+				auto const& e=edges[eid];
+				if(a[e.v]==0&&e.cap>e.flow){
+					Q.push(e.v);
+					a[e.v]=min(a[u],e.cap-e.flow);
+					p[e.v]=eid;
 				}
 			}
+			if(a[t]) break;
 		}
+		if(a[t]==0) break;
+		for(int v=t;v!=s;v=edges[p[v]].u){
+			edges[p[v]].flow+=a[t];
+			edges[p[v]^1].flow-=a[t];
+		}
+		flow+=a[t];
 	}
-	return true;
+	return flow;
 }
-class Solution {
-
-/*struct Edge{
-	int u,v;
-	int dist;
-};*/
-
-
-public:
-    int networkDelayTime(vector<vector<int>>& times, int N, int K) {
-		G.resize(N);d.resize(N,MAXINT);
-		for(auto triple: times){
-			auto u=triple[0]-1;
-			auto v=triple[1]-1;
-			auto d=triple[2];
-			//edges.push_back(Edge{u,v,d});
-			//G[u].push_back(edges.size()-1);
-			G[u].push_back(Node{v,d});
-		}
-		//return SPFA(K-1);
-    }
-};
+void readGraph(){
+	int n,m;
+	cin>>n>>m;
+	G.resize(n);a.resize(n);p.resize(n);
+	for(int i=0;i<m;++i){
+		int a,b,c;
+		cin>>a>>b>>c;
+		addEdge(a,b,c);
+	}
+}
 int main()
 {
 	freopen("input.txt","r",stdin);
 	readGraph();
-	SPFA(0);
-	printVec(d);
-	cout<<endl;
-	printVec(p);
-	return 0;
+	cout<<maxFlow(0,5)<<endl;
+	printVec(edges);
+    return 0;
 }
