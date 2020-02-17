6fe8727c80c7bbebb860b82e56ff89f965c30f33
zypang <pangzaiyu@163.com>
2020-02-17 17:28:03 +0800

    spfa

--- main.cpp
+++ main.cpp
@@ -14,21 +14,55 @@ template <typename T>
 void printVec(vector<T> const& v){
 	copy(v.begin(),v.end(),ostream_iterator<T>(cout," "));
 }
-
-/*void readGraph(){
+const int MAXINT=numeric_limits<int>::max();
+struct Node{
+	int u,d;
+};
+vector<vector<Node>> G;
+vector<int> d;
+vector<int> p;
+vector<bool> inque;
+vector<int> cnt;
+void readGraph(){
 	int m,n;
 	cin>>m>>n;
 	G.resize(m);
 	d.resize(m,MAXINT);
-	s.resize(m);
 	p.resize(m);
+	cnt.resize(m);
+	inque.resize(m);
 	for(int i=0;i<n;++i){
 		int u,v,d;
 		cin>>u>>v>>d;
-		edges.push_back(Edge{u,v,d});
-		G[u].push_back(edges.size()-1);
+		G[u].push_back(Node{v,d});
 	}
-}*/
+}
+bool SPFA(int u){
+	queue<int> q;
+	q.push(u);
+	inque[u]=1;
+	d[u]=0;
+	p[u]=-1;
+	int n=G.size();
+	while(!q.empty()){
+		auto u=q.front();q.pop();
+		inque[u]=0;
+		for(auto const& node: G[u]){
+			auto v=node.u;
+			auto dist=node.d;
+			if(d[u]<MAXINT && d[u]+dist<d[v]){
+				d[v]=dist+d[u];
+				p[v]=u;
+				if(!inque[v]){
+					q.push(v);
+					inque[v]=1;
+					if(++cnt[v]>n) return false;
+				}
+			}
+		}
+	}
+	return true;
+}
 class Solution {
 
 /*struct Edge{
@@ -36,48 +70,10 @@ class Solution {
 	int dist;
 };*/
 
-/*************** below are Dijkstra template codes ******************/
-const int MAXINT=numeric_limits<int>::max();
-struct Node{
-	int u,d;
-	bool operator<(Node const& rhs) const{
-		return d>rhs.d; // > for min root heap
-	}
-};
-//Do not forget to initialize these vectors with node numbers N
-vector<vector<Node>> G;
-vector<int> d; //Initialize as MAXINT
-vector<bool> s; 
-vector<int> p; // path: node id before
-//vector<Edge> edges;
-
-int dijkstra(int u){
-	priority_queue<Node> q;
-	d[u]=0;
-	p[u]=-1;
-	q.push(Node{u,0});
-	while(!q.empty()){
-		auto node=q.top();q.pop();
-		auto u=node.u;
-		s[u]=1;
-		for(auto node:G[u]){
-			auto v=node.u;
-			auto dist=node.d;
-			if(!s[v] && d[u]+dist<d[v]){
-			  d[v]=d[u]+dist;
-			  q.push(Node{v,d[v]});
-			  p[v]=u;
-			}
-		}
-	}
-	int maxD=*max_element(d.begin(),d.end());
-	return maxD<MAXINT?maxD:-1;
-}
-/*******************************************************************/
 
 public:
     int networkDelayTime(vector<vector<int>>& times, int N, int K) {
-		G.resize(N);d.resize(N,MAXINT);s.resize(N);
+		G.resize(N);d.resize(N,MAXINT);
 		for(auto triple: times){
 			auto u=triple[0]-1;
 			auto v=triple[1]-1;
@@ -86,16 +82,16 @@ public:
 			//G[u].push_back(edges.size()-1);
 			G[u].push_back(Node{v,d});
 		}
-		return dijkstra(K-1);
+		//return SPFA(K-1);
     }
 };
 int main()
 {
 	freopen("input.txt","r",stdin);
-	/*readGraph();
-	dijstra(0);
+	readGraph();
+	SPFA(0);
 	printVec(d);
 	cout<<endl;
-	printVec(p);*/
+	printVec(p);
 	return 0;
 }
