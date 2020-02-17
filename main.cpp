d8268154d502a0dc7f0f7d22e1b753f9942886f8
zypang <pangzaiyu@163.com>
2020-02-17 12:12:13 +0800

    topo sort two methods

--- main.cpp
+++ main.cpp
@@ -19,58 +19,66 @@ struct Edge{
 	int u,v;
 	int dist;
 };
-struct Comp{
-	bool operator()(Edge const& e1,Edge const& e2) const {
-		return e1.dist>e2.dist;
-	}
-};
-ostream& operator<<(ostream& out,Edge const& e){
-	out<<"("<<e.u<<","<<e.v<<","<<e.dist<<")";
-	return out;
-}
-priority_queue<Edge,vector<Edge>,Comp> Q;
-vector<int> fa;
-int findFather(int p){
-	if(fa[p]==p) return p;
-	return fa[p]=findFather(fa[p]);
-}
-void myUnion(int p,int q){
-	int u=findFather(p);
-	int v=findFather(q);
-	fa[v]=u;
-}
+vector<vector<int>> G;
+vector<int> vis;
+vector<int> topo;
+int t;
 int m,n;
+vector<int> degree;
+queue<int> Q;
 void readGraph(){
 	cin>>m>>n;
-	fa.resize(m);
-	for(int i=0;i<m;++i){
-		fa[i]=i;
-	}
+	t=m;
+	G.resize(m);
+	vis.resize(m);
+	topo.resize(m);
+	degree.resize(m);
 	for(int i=0;i<n;++i){
 		int u,v,d;
 		cin>>u>>v>>d;
-		Q.push(Edge{u,v,d});
+		G[u-1].push_back(v-1);
+		++degree[v-1];
 	}
 }
-vector<Edge> MST(){
-	vector<Edge> mst;
+bool topoSortDegree(){
+	topo.clear();
+	for(int i=0;i<m;++i){
+		if(degree[i]==0) Q.push(i);
+	}
 	while(!Q.empty()){
-	auto e=Q.top();Q.pop();
-	if(findFather(e.u)!=findFather(e.v)){
-		mst.push_back(move(e));
-		myUnion(e.u,e.v);
+		auto u=Q.front();Q.pop();
+		topo.push_back(u);
+		for(auto v: G[u]){
+			if(--degree[v]==0) Q.push(v);
+		}
 	}
+	if(topo.size()<m) return false;
+	return true;
+}
+bool _topoSortDFS(int u){
+	vis[u]=-1;
+	for(auto v: G[u]){
+		if(vis[v]==-1) return false;
+		if(vis[v]==0 && !_topoSortDFS(v)) return false;
 	}
-	if(mst.size()==m-1) return mst;
-	else return vector<Edge>();
+	vis[u]=1;
+	topo[--t]=u;
+	return true;
+}
+bool topoSortDFS(){
+	for(int i=0;i<m;++i){
+		if(vis[i]) continue;
+		if(!_topoSortDFS(i)) return false;
+	 }
+	return true;
 }
 const int MAXINT=numeric_limits<int>::max();
+
 int main()
 {
-
 	freopen("input.txt","r",stdin);
 	readGraph();
-	auto mst=MST();
-	printVec(mst);
+	if(topoSortDFS()) printVec(topo);
+	else cout<<"Loop"<<endl;
 	return 0;
 }
