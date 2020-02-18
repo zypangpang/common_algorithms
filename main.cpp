b8df2297287cf37c964682873fc5f9fda0c13a26
zypang <pangzaiyu@163.com>
2020-02-18 17:22:29 +0800

    merge two sorted list

--- main.cpp
+++ main.cpp
@@ -14,67 +14,70 @@ template <typename T>
 void printVec(vector<T> const& v){
 	copy(v.begin(),v.end(),ostream_iterator<T>(cout," "));
 }
-const int MAXINT=numeric_limits<int>::max();
-struct Edge{
-	int u,v,cap,flow;
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
 };
-ostream& operator<<(ostream& out, Edge const& e){
-	cout<<"("<<e.u<<","<<e.v<<","<<e.cap<<","<<e.flow<<")";
-	return out;
+ListNode* createList(vector<int> const&  v){
+	auto head=new ListNode(-1);
+	auto p=head;
+	for(auto a:v){
+		p->next=new ListNode(a);
+		p=p->next;
+	}
+	p=head->next;
+	delete head;
+	return p;
 }
-vector<Edge> edges;
-vector<vector<int>> G;
-vector<int> a;
-vector<int> p;
-void addEdge(int u,int v,int c){
-	edges.push_back(Edge{u,v,c,0});
-	edges.push_back(Edge{v,u,0,0});
-	G[u].push_back(edges.size()-2);
-	G[v].push_back(edges.size()-1);
+void printList(ListNode* head){
+	for(;head!=NULL;head=head->next){
+		cout<<head->val<<" ";
+	}
 }
-int maxFlow(int s,int t){
-	int flow=0;
-	while(1){
-		fill(a.begin(),a.end(),0);
-		a[s]=MAXINT;
-		queue<int> Q;
-		Q.push(s);
-		while(!Q.empty()){
-			auto u=Q.front();Q.pop();
-			for(auto eid: G[u]){
-				auto const& e=edges[eid];
-				if(a[e.v]==0&&e.cap>e.flow){
-					Q.push(e.v);
-					a[e.v]=min(a[u],e.cap-e.flow);
-					p[e.v]=eid;
-				}
+
+class Solution {
+public:
+    ListNode* mergeTwoListsRecur(ListNode* l1, ListNode* l2) {
+		if(l1==NULL) return l2;
+		if(l2==NULL) return l1;
+		if(l1->val<l2->val){
+			l1->next=mergeTwoListsRecur(l1->next,l2);
+			return l1;
+		}else{
+			l2->next=mergeTwoListsRecur(l1,l2->next);
+			return l2;
+		}
+	}
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+		auto p=l1,q=l2;
+		auto c=new ListNode(-1);
+		c->next=l1;
+		auto guard=c;
+		while(p!=NULL||q!=NULL){
+			if(p==NULL || q!=NULL && p->val>q->val){
+				auto t=q;
+				q=q->next;
+				c->next=t;
+				t->next=p;
+				c=t;
 			}
-			if(a[t]) break;
+			else {
+				p=p->next;
+				c=c->next;
+			}
 		}
-		if(a[t]==0) break;
-		for(int v=t;v!=s;v=edges[p[v]].u){
-			edges[p[v]].flow+=a[t];
-			edges[p[v]^1].flow-=a[t];
-		}
-		flow+=a[t];
-	}
-	return flow;
-}
-void readGraph(){
-	int n,m;
-	cin>>n>>m;
-	G.resize(n);a.resize(n);p.resize(n);
-	for(int i=0;i<m;++i){
-		int a,b,c;
-		cin>>a>>b>>c;
-		addEdge(a,b,c);
-	}
-}
+		return guard->next;
+    }
+};
 int main()
 {
-	freopen("input.txt","r",stdin);
-	readGraph();
-	cout<<maxFlow(0,5)<<endl;
-	printVec(edges);
+	vector<int> v1{1,6,7};
+	vector<int> v2{1,2,3};
+	//freopen("input.txt","r",stdin);
+	//readGraph();
+	Solution solution;
+	auto v=solution.mergeTwoListsRecur(createList(v1),createList(v2));
+	printList(v);
     return 0;
 }
