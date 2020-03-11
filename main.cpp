ef4a7a8dcb985d10c8c30d2099197b91187c56c6
zypang <pangzaiyu@163.com>
2020-03-11 20:50:16 +0800

    leetcode 84 largest rectangle in histogram: find first left smaller and right smaller

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 #include <chrono>
 #include <array>
 #include <cmath>
+#include <stack>
 
 using namespace std;
 
@@ -37,50 +38,73 @@ void printVec(vector<T> const& v){
 	copy(v.begin(),v.end(),ostream_iterator<T>(cout," "));
 }
 class Solution {
-	vector<vector<int>> ans;
-	void _subset(int k,vector<int> const& nums,vector<int>& res){
-		if(k>=nums.size()){
-			ans.push_back(res);
+	vector<int> tree;
+	void buildTree(vector<int> const& h,int l,int r,int node){
+        if(l>=r) return;
+		if(l+1==r){
+			tree[node]=l;
 			return;
 		}
-		_subset(k+1,nums,res);
-		res.push_back(nums[k]);
-		_subset(k+1,nums,res);
-		res.pop_back();
+		int mid=l+((r-l)>>1);
+		buildTree(h,l,mid,node<<1);
+		buildTree(h,mid,r,node<<1|1);
+		tree[node]=h[tree[node<<1]]<=h[tree[node<<1|1]]?tree[node<<1]:tree[node<<1|1];
 	}
-	vector<int> getSubset(int k,vector<int> const& s){
-		vector<int> ans;
-		for(int i=0;i<s.size();++i){
-			if(k&1<<i) ans.push_back(s[i]);
+	int getMinFast(vector<int> const& h,int l,int r,int node,int L,int R){
+		if(L<=l&&R>=r) return tree[node];
+		int resL=-1,resR=-1;
+		int mid=l+((r-l)>>1);
+		if(mid>L) resL=getMinFast(h,l,mid,node<<1,L,R);
+		if(mid<R)  resR=getMinFast(h,mid,r,node<<1|1,L,R);
+		if(resL!=-1&& resR!=-1) return h[resL]<=h[resR]? resL:resR;
+		if(resL!=-1) return resL;
+		return resR;
+	}
+	int getMin(vector<int> const& h, int l,int r){
+		int minE=h[l];
+		int minId=l;
+		for(int i=l+1;i<r;++i)
+		  if(h[i]<minE){
+			  minE=h[i];
+			  minId=i;
+		  }
+		return minId;
+	}
+	int _getArea(vector<int> const& h,int l,int r){
+		if(l>=r) return 0;
+		int minId=getMinFast(h,0,h.size(),1,l,r);
+		int cur=h[minId]*(r-l);
+		return max(cur,max(_getArea(h,l,minId),_getArea(h,minId+1,r)));
+	}
+	//Stack approach. Essentially, find left first smaller and right first smaller element.
+	int getAreaStack(vector<int>& ht){
+		stack<int> stk;
+		vector<int> h;
+		h.push_back(0);
+		h.insert(h.end(),move(ht.begin()),move(ht.end()));
+		h.push_back(0);
+		int res=0;
+		for(int i=0;i<h.size();++i){
+			//Actually, > only guarantee the right first smaller one, the left is left first smaller or equal one.
+			while(!stk.empty()&& h[stk.top()]>h[i]){
+				int tmp=stk.top();stk.pop();
+				res=max(res,(i-1-stk.top())*h[tmp]);
+			}
+			stk.push(i);
 		}
-		return ans;
+		return res;
 	}
 public:
-	vector<vector<int>> subsetsRecur(vector<int>& nums){
-		vector<int> res;
-		_subset(0,nums,res);
-		return ans;
-	}
-
-    vector<vector<int>> subsetsIter(vector<int>& nums) {
-		vector<vector<int>> ans;
-		ans.push_back(vector<int>());
-		for(auto cur: nums){
-			vector<vector<int>> t=ans;
-			for(auto& x: ans){
-				x.push_back(cur);
-			}
-			ans.insert(ans.end(),move(t.begin()),move(t.end()));
-		}
-		
-		return ans;
+    int largestRectangleArea(vector<int>& heights) {
+		if(heights.empty()) return 0;
+		int n=heights.size();
+		/*tree.resize(4*n);
+		buildTree(heights,0,n,1);
+		int res=0;
+		res=_getArea(heights,0,n);*/
+		int res=getAreaStack(heights);
+		return res;
     }
-	vector<vector<int>> subsetBit(vector<int>& nums){
-		for(int i=0;i<1<<nums.size();++i){
-			ans.push_back(getSubset(i,nums));
-		}
-		return ans;
-	}
 };
 
 int main()
@@ -90,14 +114,12 @@ int main()
 
     //istream_iterator<string> iit(cin),eit;
     //copy(iit,eit,back_inserter(testCases));
-	vector<int> a{1,2,3};
-
+	
+	//vector<int> heights{2,1,5,6,2,3};
+	vector<int> heights {0,0,0,0,0,0,0,0,2147483647};
 	Solution sol;
-	auto const& x=sol.subsetBit(a);
-	for(auto const& v: x){
-		printVec(v);
-		cout<<endl;
-	}
+	auto x=sol.largestRectangleArea(heights);
+	cout<<x<<endl;
 
     return 0;
 }
