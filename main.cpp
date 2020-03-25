b8cedc49fb2e5b83c2a4685a72e84ed5086b79de
zypang <pangzaiyu@163.com>
2020-03-25 20:44:43 +0800

    alibaba online test

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include <array>
 #include <cmath>
 #include <stack>
+#include <queue>
 
 using namespace std;
 
@@ -37,71 +38,125 @@ template <typename T>
 void printVec(vector<T> const& v){
 	copy(v.begin(),v.end(),ostream_iterator<T>(cout," "));
 }
-struct TreeNode {
-    int val;
-    TreeNode *left;
-    TreeNode *right;
-    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-TreeNode* buildTree(vector<int> const& v,int& i){
-	if(v[i]==-1)
-	  return NULL;
-	auto root=new TreeNode(v[i]);
-	root->left=buildTree(v,++i);
-	root->right=buildTree(v,++i);
-	return root;
-}
-class Solution {
-	bool isValid(TreeNode* root,long long& minE,long long& maxE){
-		if(root==NULL){
-			minE=numeric_limits<long long>::max();
-			maxE=numeric_limits<long long>::min();
-			return true;
-		}
-		long long minL,maxL,minR,maxR;
-		auto l=isValid(root->left,minL,maxL);
-		auto r=isValid(root->right,minR,maxR);
-		if(!l||!r) return false;
-		minE=min((long long)(root->val),min(minL,minR));
-		maxE=max((long long)(root->val),max(maxL,maxR));
-		if(maxL<root->val&&minR>root->val)
-		  return true;
-		else return false;
-	}
-public:
-    bool isValidBST(TreeNode* root) {
-		//long long a,b;
-		long long cur=numeric_limits<long long>::min();
-		//return isValid(root,a,b);
-		return inOrder(root,cur);
+
+int getMinSub(vector<vector<int>> const& mat,int n){
+    int res=0;
+    for(int i=0;i<n-1;++i){
+        int minVal=numeric_limits<int>::max();
+        for(int j=0;j<3;++j){
+            for(int k=0;k<3;++k){
+                if(abs(mat[j][i]-mat[k][i+1])<minVal){
+                    minVal=abs(mat[j][i]-mat[k][i+1]);
+                }
+            }
+        }
+        res+=minVal;
     }
-	bool inOrder(TreeNode* root,long long& cur){
-		if(root==NULL) return true;
-		if(!inOrder(root->left,cur)) return false;
-		if(root->val<=cur) return false;
-		cur=root->val;
-		if(!inOrder(root->right,cur)) return false;
-		return true;
-	}
-};
+}
+long long getMin(vector<vector<int>> const& mat, int n ){
+    vector<long long> minVal(3);
+    for(int i=1;i<n;++i){
+        vector<long long> t(3,numeric_limits<int>::max());
+        for(int j=0;j<3;++j){
+            for(int k=0;k<3;++k){
+                long long x=abs((long long)mat[j][i]-(long long)mat[k][i-1])+minVal[k];
+                if(x<t[j]){
+                    t[j]=x;
+                }
+            }
+        }
+        minVal=t;
+        //printVec(minVal);
+        //cout<<endl;
+    }
+    return min(minVal[0],min(minVal[1],minVal[2]));
+}
+void fix(vector<vector<int>>& mat){
+    int n=mat.size();
+    int m=mat[0].size();
+    vector<long long> row(n,numeric_limits<long long>::max());
+    vector<long long> col(m,numeric_limits<long long>::max());
+    for(int i=0;i<n;++i){
+        for(int j=0;j<m-1;++j){
+            if(mat[i][j]!=0&&mat[i][j+1]!=0){
+                row[i]=(long long)mat[i][j+1]-(long long)mat[i][j];
+            }
+        }
+    }
+    for(int j=0;j<m;++j){
+        for(int i=0;i<n-1;++i){
+            if(mat[i+1][j]!=0&&mat[i][j]!=0){
+                col[j]=(long long)mat[i+1][j]-(long long)mat[i][j];
+            }
+        }
+    }
+    for(int i=0;i<n;++i){
+        if(row[i]!=numeric_limits<long long>::max()){
+            int k=-1;
+            for(k=0;k<m;++k){
+                if(mat[i][k]!=0)
+                    break;
+            }
+            if(k!=-1){
+                for(int j=k-1;j>=0;--j){
+                    mat[i][j]=mat[i][j+1]-row[i];
+                }
+                for(int j=k+1;j<m;++j){
+                    mat[i][j]=mat[i][j-1]+row[i];
+                }
+            }
+        }
+    }
+    for(int j=0;j<m;++j){
+        if(col[j]!=numeric_limits<long long>::max()){
+            int k=-1;
+            for(k=0;k<n;++k){
+                if(mat[k][j]!=0)
+                    break;
+            }
+            if(k!=-1){
+                for(int i=k-1;i>=0;--i){
+                    mat[i][j]=mat[i+1][j]-col[j];
+                }
+                for(int i=k+1;i<n;++i){
+                    mat[i][j]=mat[i-1][j]+col[j];
+                }
+            }
+        }
+    }
+}
 int main()
 {
 
-//[3,null,30,10,null,null,15,null,45]
     //Stdin redirect
     //freopen("input.txt","r",stdin);
 
     //istream_iterator<string> iit(cin),eit;
     //copy(iit,eit,back_inserter(testCases));
-	
-	//vector<int> v{2,1,-1,-1,3,-1,-1};
-	vector<int> v{3,-1,30,10,-1,15,-1,45,-1,-1,-1};
-	//vector<int> v{5,1,-1,-1,4,3,-1,-1,6,-1,-1};
-	int i=0;
-	auto root=buildTree(v,i);
-	Solution sol;
-	auto x=sol.isValidBST(root);
-	cout<<x<<endl;
+    int n,m,q;
+    cin>>n>>m>>q;
+    vector<vector<int>> mat(n);
+    for(int i=0;i<n;++i){
+        for(int j=0;j<m;++j){
+            int t=0;
+            cin>>t;
+            mat[i].push_back(t);
+        }
+    }
+    fix(mat);
+    /*for(auto & x: mat){
+        printVec(x);
+        cout<<endl;
+    }*/
+    for(int i=0;i<q;++i){
+        int x,y;
+        cin>>x>>y;
+        if(mat[x][y]==0)
+            cout<<"Unknown"<<endl;
+        else {
+            cout<<mat[x][y]<<endl;
+        }
+    }
 
     return 0;
 }
